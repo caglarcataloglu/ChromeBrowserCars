04541e0f3889b573a685d8c1a7f087a3df23d360
CACA <CACA@CACA-PC>
2022-03-13 21:19:54 +0300

    Add project files.

---
+++ ChromeBrowserCars/Form1.Designer.cs
@@ -0,0 +1,124 @@
+﻿
+namespace ChromeBrowserCars
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnBack = new System.Windows.Forms.Button();
+            this.btnForward = new System.Windows.Forms.Button();
+            this.btnRefresh = new System.Windows.Forms.Button();
+            this.btnGo = new System.Windows.Forms.Button();
+            this.txtUrl = new System.Windows.Forms.TextBox();
+            this.pContainer = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // btnBack
+            // 
+            this.btnBack.Location = new System.Drawing.Point(35, 22);
+            this.btnBack.Name = "btnBack";
+            this.btnBack.Size = new System.Drawing.Size(75, 23);
+            this.btnBack.TabIndex = 0;
+            this.btnBack.Text = "<";
+            this.btnBack.UseVisualStyleBackColor = true;
+            this.btnBack.Click += new System.EventHandler(this.btnBack_Click);
+            // 
+            // btnForward
+            // 
+            this.btnForward.Location = new System.Drawing.Point(116, 22);
+            this.btnForward.Name = "btnForward";
+            this.btnForward.Size = new System.Drawing.Size(75, 23);
+            this.btnForward.TabIndex = 1;
+            this.btnForward.Text = ">";
+            this.btnForward.UseVisualStyleBackColor = true;
+            this.btnForward.Click += new System.EventHandler(this.btnForward_Click);
+            // 
+            // btnRefresh
+            // 
+            this.btnRefresh.Location = new System.Drawing.Point(197, 22);
+            this.btnRefresh.Name = "btnRefresh";
+            this.btnRefresh.Size = new System.Drawing.Size(75, 23);
+            this.btnRefresh.TabIndex = 2;
+            this.btnRefresh.Text = "Refresh";
+            this.btnRefresh.UseVisualStyleBackColor = true;
+            this.btnRefresh.Click += new System.EventHandler(this.btnRefresh_Click);
+            // 
+            // btnGo
+            // 
+            this.btnGo.Location = new System.Drawing.Point(888, 23);
+            this.btnGo.Name = "btnGo";
+            this.btnGo.Size = new System.Drawing.Size(75, 23);
+            this.btnGo.TabIndex = 3;
+            this.btnGo.Text = "Go";
+            this.btnGo.UseVisualStyleBackColor = true;
+            this.btnGo.Click += new System.EventHandler(this.btnGo_Click);
+            // 
+            // txtUrl
+            // 
+            this.txtUrl.Location = new System.Drawing.Point(278, 24);
+            this.txtUrl.Name = "txtUrl";
+            this.txtUrl.Size = new System.Drawing.Size(604, 20);
+            this.txtUrl.TabIndex = 4;
+            // 
+            // pContainer
+            // 
+            this.pContainer.Location = new System.Drawing.Point(44, 56);
+            this.pContainer.Name = "pContainer";
+            this.pContainer.Size = new System.Drawing.Size(918, 645);
+            this.pContainer.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1008, 729);
+            this.Controls.Add(this.pContainer);
+            this.Controls.Add(this.txtUrl);
+            this.Controls.Add(this.btnGo);
+            this.Controls.Add(this.btnRefresh);
+            this.Controls.Add(this.btnForward);
+            this.Controls.Add(this.btnBack);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Chromium v1.0";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnBack;
+        private System.Windows.Forms.Button btnForward;
+        private System.Windows.Forms.Button btnRefresh;
+        private System.Windows.Forms.Button btnGo;
+        private System.Windows.Forms.TextBox txtUrl;
+        private System.Windows.Forms.Panel pContainer;
+    }
+}
+
---
+++ ChromeBrowserCars/Form1.cs
@@ -0,0 +1,167 @@
+﻿using CefSharp;
+using CefSharp.WinForms;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ChromeBrowserCars
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        ChromiumWebBrowser chrome;
+        static bool isLoggedIn = false;
+
+        private async void Form1_Load(object sender, EventArgs e) //async evaluatescript icin
+        {
+            CefSettings settings = new CefSettings();
+            Cef.Initialize(settings);
+            txtUrl.Text = "https://www.cars.com/signin/?redirect_path=%2F";
+            chrome = new ChromiumWebBrowser(txtUrl.Text);
+            
+            this.pContainer.Controls.Add(chrome);
+            chrome.Dock = DockStyle.Fill;
+            chrome.AddressChanged += Chrome_AddressChanged;
+            chrome.LoadingStateChanged += BrowserLoadingStateChangedAsync;         
+
+        }
+
+        private void BrowserLoadingStateChangedAsync(object sender, LoadingStateChangedEventArgs e)
+        {
+            if (!e.IsLoading)
+            {
+                if (!isLoggedIn)
+                {
+                    // enter user and password, then submit the form with class name
+                    var loginScript = @"document.querySelector('#email').value = 'johngerson808@gmail.com';
+                               document.querySelector('#password').value = 'test8008';
+                               document.querySelector('.session-form').submit();";
+
+                    chrome.EvaluateScriptAsync(loginScript).ContinueWith(u =>
+                    {                                       
+                        isLoggedIn = true;
+                        Console.WriteLine("Succesfully Logged In.\n");
+                    });
+                }
+                else 
+                {
+                    Console.WriteLine("Problem escalated for Loggin Operation.\n");
+                }
+                var filterScript = @"document.querySelector('#make-model-search-stocktype').value = 'used';
+                               document.querySelector('#makes').value = 'tesla';
+                               document.querySelector('#makes').dispatchEvent(new Event('change', { 'bubbles': true }));
+                               document.querySelector('#models').value = 'tesla-model_s';
+                               document.querySelector('#models').dispatchEvent(new Event('change', { 'bubbles': true }));
+                               document.querySelector('#make-model-max-price').value = '100000';               
+                               document.querySelector('#make-model-maximum-distance').value = 'all';
+                               document.querySelector('#make-model-zip').value = '94596';
+                               document.querySelector('.sds-button').click();";
+
+                //JavascriptResponse response1 = await chrome.EvaluateScriptAsync("d");
+                //var task = chrome.EvaluateScriptAsync("(select() { return document.getElementById('makes').value; })();", "tesla");
+
+                var jsScript = @"
+
+                var allList = document.querySelector('#vehicle-cards-container')
+                var carList = allList.getElementsByClassName('vehicle-card-main js-gallery-click-card');
+                var jsons = new Array();
+                    function replacer(key, value) {
+                    return typeof value === 'undefined' ? null : value;
+                }
+                for (var i = 0; i < carList.length; i++) {       
+                    var title = carList[i].getElementsByClassName('title');
+                    var mileage = carList[i].getElementsByClassName('mileage');
+                    var primary_price = carList[i].getElementsByClassName('primary-price');
+                    var dealer_name = carList[i].getElementsByClassName('dealer-name');
+                    var rating = carList[i].getElementsByClassName('sds-rating__count');
+                    var miles_from = carList[i].getElementsByClassName('miles-from');
+                    var badge = carList[i].getElementsByClassName('sds-badge__label');
+        
+                    if(badge[1]==null)
+                        var badge1=undefined
+                    else
+                        var badge1=badge[1].innerText
+                    if(badge[2]==null)
+                        var badge2=undefined
+                    else
+                        var badge2 = badge[2].innerText
+                    if(badge[3]==null)
+                        var badge3=undefined
+                    else
+                        var badge3=badge[3].innerText
+    
+                    var jsonData = JSON.stringify({ 
+                            title: title[0].innerHTML, 
+                            mileage: mileage[0].innerHTML,
+                            primary_price: primary_price[0].innerHTML,
+                            dealer_name: dealer_name[0].innerText,
+                            rating: rating[0].innerHTML,
+                            miles_from: miles_from[0].innerHTML,
+                            badge: [badge[0].innerText,badge1,badge2,badge3]
+                        }, replacer, '\t', 4);
+
+                    console.log(jsonData);
+                    jsons.push(jsonData)
+                    jsons.concat('\n')
+                } 
+                    console.log(jsons);
+            ";
+
+                chrome.ExecuteScriptAsyncWhenPageLoaded(filterScript);
+                Console.WriteLine("filterScript is executed.\n");
+
+                chrome.EvaluateScriptAsync(jsScript).ContinueWith(u =>
+                { Console.WriteLine(u.Result.Message.ToString()); 
+                //return bilgileri dosyaya yazilacak.
+                }
+                );
+
+            }
+        }
+
+        private void Chrome_AddressChanged(object sender, AddressChangedEventArgs e)
+        {
+            this.Invoke(new MethodInvoker(() => {
+                txtUrl.Text = e.Address;
+            }));
+        }
+
+        private void btnGo_Click(object sender, EventArgs e)
+        {
+            chrome.Load(txtUrl.Text);
+            
+        }
+
+
+        private void btnBack_Click(object sender, EventArgs e)
+        {
+            if (chrome.CanGoBack)
+                chrome.Back();
+        }
+
+        private void btnForward_Click(object sender, EventArgs e)
+        {
+            if (chrome.CanGoForward)
+                chrome.Forward();
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Cef.Shutdown();
+        }
+
+        private void btnRefresh_Click(object sender, EventArgs e)
+        {
+            chrome.Reload(true);
+        }
+    }
+}
